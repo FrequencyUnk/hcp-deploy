4e818e4a53c96f5e41c1b222b3ddae7a26172059
FrequencyUnk <77994041+FrequencyUnk@users.noreply.github.com>
2021-09-24 18:42:39 -0500

    Create hcp.tf
    Used to create initial hcp infrastructure

---
+++ hcp.tf
@@ -0,0 +1,19 @@
+resource "hcp_hvn" "deploy_hcp_hvn" {
+  hvn_id         = var.hvn_id
+  cloud_provider = var.cloud_provider
+  region         = var.region
+}
+
+resource "hcp_vault_cluster" "deploy_hcp_vault" {
+  hvn_id     = hcp_hvn.deploy_hcp_hvn.hvn_id
+  cluster_id = var.vault_cluster_id
+  tier       = var.vault_tier
+  public_endpoint = true
+}
+
+resource "hcp_consul_cluster" "deploy_hcp_consul" {
+  hvn_id          = hcp_hvn.deploy_hcp_hvn.hvn_id
+  cluster_id      = var.consul_cluster_id
+  tier            = var.consul_tier
+  public_endpoint = true
+}
