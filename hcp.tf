b8c46ee581c7663d60820a5b359c51bd808ec1bb
FrequencyUnk <77994041+FrequencyUnk@users.noreply.github.com>
2021-09-24 19:44:37 -0500

    Update hcp.tf

--- hcp.tf
+++ hcp.tf
@@ -14,6 +14,6 @@ resource "hcp_vault_cluster" "deploy_hcp_vault" {
 resource "hcp_consul_cluster" "deploy_hcp_consul" {
   hvn_id          = hcp_hvn.deploy_hcp_hvn.hvn_id
   cluster_id      = var.consul_cluster_id
-  tier            = var.consul_tier
+  tier            = "dev"
   public_endpoint = true
 }
